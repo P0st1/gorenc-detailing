70445d68b5b671fb63f16a1b5a8c1b870e6d8c5c
Gregor Kramer <kramergregorr@gmail.com>
2024-01-09 16:09:11 +0100

    trying to publish

--- gorencdetailing/gorencdetailing/settings.py
+++ gorencdetailing/gorencdetailing/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qk)aqjgz^qbq1yrzf_bw#yf(!hbdmk$!c6od7c^29x2pz47d5$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['gorenc-detailing.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,5 +134,3 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'detailing.gorenc@gmail.com'
 EMAIL_HOST_PASSWORD = 'xhth lwgo mlya glid'
 
-import django_heroku
-django_heroku.settings(locals())
